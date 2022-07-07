fcf18c5bc317e11b4ad6963495c9b4507dc9891c
Mikhail Trapeznikov <trapma@yandex.ru>
2022-07-07 20:49:45 +1000

    refactor PostService

--- src/API/PostService.js
+++ src/API/PostService.js
@@ -5,11 +5,7 @@ const API_HOST = config.api_host;
 
 export default class PostService {
   static async getAll() {
-    try {
-      const response = await axios.get(`${API_HOST}/posts`);
-      return response.data;
-    } catch (e) {
-      console.log(e);
-    }
+    const response = await axios.get(`${API_HOST}/posts`);
+    return response.data;
   }
 }
