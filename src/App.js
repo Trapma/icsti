8c5686981769958e6df673987d21dae5186ac470
Mikhail Trapeznikov <trapma@yandex.ru>
2022-07-07 20:27:17 +1000

    connect get all posts on App

--- src/App.js
+++ src/App.js
@@ -1,27 +1,20 @@
 import "./App.css";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import Navigation from "./components/Navigation";
 import Header from "./components/Header";
 import CardList from "./components/CardList";
+import PostService from "./API/PostService";
 
 function App() {
-  const [posts, setPosts] = useState([
-    {
-      id: 1,
-      title: "Проверка заголовка 1",
-      body: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt praesentium, fugiat id vero enim harum consequatur reiciendis cupiditate quidem consectetur facere totam! Cupiditate fugiat necessitatibus dolores, neque vel tempora tempore! ",
-    },
-    {
-      id: 2,
-      title: "Проверка заголовка 2",
-      body: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt praesentium, fugiat id vero enim harum consequatur reiciendis cupiditate quidem consectetur facere totam! Cupiditate fugiat necessitatibus dolores, neque vel tempora tempore! ",
-    },
-    {
-      id: 3,
-      title: "Проверка заголовка 3",
-      body: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt praesentium, fugiat id vero enim harum consequatur reiciendis cupiditate quidem consectetur facere totam! Cupiditate fugiat necessitatibus dolores, neque vel tempora tempore! ",
-    },
-  ]);
+  const [posts, setPosts] = useState([]);
+  useEffect(() => {
+    fetchPosts();
+  }, []);
+
+  async function fetchPosts() {
+      const posts = await PostService.getAll();
+      setPosts(posts);
+  }
 
   return (
     <div className="App">
