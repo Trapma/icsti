3fc93b0fca0338ed3a729fd84e1a75e30cd5708a
Mikhail Trapeznikov <trapma@yandex.ru>
2022-07-11 04:24:27 +1000

    add Home (about) route

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,8 @@ import "./App.css";
 import { Outlet, useLocation } from "react-router-dom";
 import React from "react";
 import Navigation from "./components/Navigation";
-import Header from "./components/Header";
 import Footer from "./components/Footer";
+import Home from "./routes/Home";
 
 function App() {
   let location = useLocation();
@@ -11,7 +11,10 @@ function App() {
   return (
     <div className="App">
       <Navigation />
-      {location.pathname === "/" && <Header search={false} title="МЦНТИ" />}
+
+      {location.pathname === "/" &&
+        <Home />
+      }
       <Outlet />
       <Footer />
     </div>
